44872fd7fc3bfbc98c3840d4eaa10ba70ae04736
Paulo Pires <taralloqueiroiz@gmail.com>
2013-10-28 17:45:37 -0200

    tentando converter o array :P

--- converter.js
+++ converter.js
@@ -6,6 +6,7 @@
 
 var program = require('commander');
 var fs = require('fs');
+var _ = require('underscore');
 
 program
     .version('0.0.1')
@@ -14,25 +15,44 @@ program
     .parse(process.argv);
 
 /**/
-if (program.to_object) console.log(' - object js ');
+if (program.to_object) console.log('- object js ');
 if (program.to_xls) {
-	var dados = [
-		['Nome',   'Sobrenome', 'Profissão'],
-		['Paula',  'Tejando',   'Adivinha' ],
-		['Cuca',   'Beludo',    'Padeiro'  ],
-		['Thomas', 'Turbano',   'Popeye'   ]
-	];
+    function toArray(_Object){
+        var _Array = new Array();
+        for(var name in _Object){
+            _Array[name] = _Object[name];
+        }
+         return _Array;
+    }
+
+    var myObj = {
+        name:"Paulo",
+        age: 20,
+        status:"Enamorado"
+    };
+
+    var array = toArray(myObj);
+
+    console.log(array);
+
+    fs.readFile('translations.js', 'utf8', function(err, data){
+
+        console.log('data');
+
+	/*var dados = [];
 
 	var csv = dados.map(function(linha){
 		return linha.map(function(celula){
 			return '"'+(celula.replace(/\"/g, '\"').replace(/\r/g, '\\r'))+'"';
 		}).join(';');
-	}).join('\n');
+	}).join('\n');*/
 
-	fs.writeFile('translate.csv', csv, function(err){
+    });
+
+	/*fs.writeFile('translate.csv', csv, function(err){
 		if (err) throw err;
 		console.log('It\'s saved!');
 	});
 
-	console.log(csv);
+	console.log(csv);*/
 };
